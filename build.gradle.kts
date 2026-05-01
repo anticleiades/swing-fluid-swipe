990500dc3290dad5af8cf04abaf5af6060307a67
Giuliano <software@giulianogorgone.eu>
2026-05-01 12:03:01 +0200

    Update Gradle and Maven publishing configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     idea
-    signing
     `java-library`
-    `maven-publish`
+    id("com.vanniktech.maven.publish") version "0.36.0"
 }
 
 val accountName: String by properties
@@ -17,103 +16,73 @@ val pubDescription: String by properties
 val pubAuthor: String by properties
 val pubLicenseName: String by properties
 
-val ossrhUsername: String by properties
-val ossrhPassword: String by properties
-
 allprojects {
     group = pubGroupID
     version = pubVersion
-    apply(plugin = "signing")
-    apply(plugin = "maven-publish")
-
-    configure<SigningExtension> {
-        val publishing = extensions["publishing"] as PublishingExtension
-        sign(publishing.publications)
-    }
 
     plugins.withType<JavaPlugin> {
         configure<JavaPluginExtension> {
             sourceCompatibility = JavaVersion.VERSION_1_9
             targetCompatibility = JavaVersion.VERSION_1_9
-            withSourcesJar()
-            withJavadocJar()
         }
 
-        tasks {
-            withType<Javadoc>().configureEach {
-                (options as StandardJavadocDocletOptions).apply {
-                    locale = "en"
-                    docEncoding = "UTF-8"
-                    charSet = "UTF-8"
-                    encoding = "UTF-8"
-                    docTitle = project.name
-                    windowTitle = project.name
-                    header = "<b>${pubName}</b>"
-                    quiet()
-                    addStringOption("-add-exports", "java.desktop/sun.awt=fluidswipe.utils")
-                    addStringOption("source", "9")
-                    addBooleanOption("html5", true)
-                    links("https://docs.oracle.com/javase/9/docs/api/")
+        tasks.withType<Javadoc>().configureEach {
+            (options as StandardJavadocDocletOptions).apply {
+                locale = "en"
+                docEncoding = "UTF-8"
+                charSet = "UTF-8"
+                encoding = "UTF-8"
+                docTitle = project.name
+                windowTitle = project.name
+                header = "<b>${pubName}</b>"
+                quiet()
+                // Mantiene il bypass di Jigsaw per accedere alle classi interne di AWT
+                addStringOption("-add-exports", "java.desktop/sun.awt=fluidswipe.utils")
+                addStringOption("source", "9")
+                addBooleanOption("html5", true)
+                links("https://docs.oracle.com/javase/9/docs/api/")
+            }
+        }
+    }
+}
+
+subprojects {
+    apply(plugin = "com.vanniktech.maven.publish")
+
+    mavenPublishing {
+        coordinates(pubGroupID, project.name, pubVersion)
+
+        pom {
+            name.set(project.name)
+            description.set(pubDescription)
+            url.set("https://${scmUrlRoot}/${accountName}/${pubName}")
+
+            licenses {
+                license {
+                    name.set(pubLicenseName)
+                    url.set("https://${scmUrlRoot}/${accountName}/${pubName}/blob/master/LICENSE")
                 }
+            }
+            developers {
+                developer {
+                    id.set(accountName)
+                    name.set(pubAuthor)
+                    email.set(pubMail)
+                }
+            }
+            issueManagement {
+                system.set(issueManagementSys)
+                url.set("https://${scmUrlRoot}/${accountName}/${pubName}/issues")
+            }
+            scm {
+                connection.set("scm:git:git://${scmUrlRoot}/${accountName}/${pubName}.git")
+                developerConnection.set("scm:git:ssh://git@github.com:${accountName}/${pubName}.git")
+                url.set("https://${scmUrlRoot}/${accountName}/${pubName}")
             }
         }
 
-        publishing {
-            if (project.path == ":") {
-                // Skip the root project
-                return@publishing
-            }
-            publications {
-                create<MavenPublication>(project.name) {
-                    groupId = pubGroupID
-                    artifactId = project.name
-                    version = pubVersion
-                    description = project.description
-                    from(project.components["java"])
-                    pom {
-                        name.set(project.name)
-                        description.set(pubDescription)
-                        url.set("https://${scmUrlRoot}/${accountName}/${pubName}")
-                        licenses {
-                            license {
-                                name.set(pubLicenseName)
-                                url.set("https://${scmUrlRoot}/${accountName}/${pubName}/blob/master/LICENSE")
-                            }
-                        }
-                        developers {
-                            developer {
-                                id.set(accountName)
-                                name.set(pubAuthor)
-                                email.set(pubMail)
-                            }
-                        }
-                        issueManagement {
-                            system.set(issueManagementSys)
-                            url.set("https://${scmUrlRoot}/${accountName}/${pubName}/issues")
-                        }
-                        scm {
-                            url.set("https://${scmUrlRoot}/${accountName}/${pubName}")
-                            connection.set("scm:git:git://${scmUrlRoot}/${accountName}/${pubName}.git")
-                            developerConnection.set("scm:git:ssh://git@github.com:${accountName}/${pubName}.git")
-                        }
-                    }
-                }
-            }
-            repositories {
-                maven {
-                    name = "ossrh"
+        publishToMavenCentral(automaticRelease = false)
 
-                    val isSnapshot = version.toString().endsWith("SNAPSHOT")
-
-                    url = if (isSnapshot) uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-                    else uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-
-                    credentials {
-                        username = ossrhUsername
-                        password = ossrhPassword
-                    }
-                }
-            }
-        }
+        signAllPublications()
     }
 }
